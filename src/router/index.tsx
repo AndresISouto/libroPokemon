e7cbb3f2386e4acc8471de8759bfa313f8977956
AndresISouto <AndresISouto@users.noreply.github.com>
2026-02-05 10:09:18 +0100

    router

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,16 +1,33 @@
 import { createBrowserRouter } from "react-router";
 import { RootLayout } from "../layouts/RootLayout";
 import { Homepage } from "../pages/Homepage";
+import { GlosaryPage } from "@/pages/GlosaryPage";
 
 export const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <RootLayout></RootLayout>,
     children: [
       {
         index: true,
         element: <Homepage></Homepage>,
       },
+      {
+        path: "/glosario/pokemons",
+        element: <GlosaryPage></GlosaryPage>,
+      },
+      {
+        path: "/glosario/personajes",
+        element: <GlosaryPage></GlosaryPage>,
+      },
+      {
+        path: "/glosario/mapas",
+        element: <GlosaryPage></GlosaryPage>,
+      },
+      {
+        path: "/glosario/lore",
+        element: <GlosaryPage></GlosaryPage>,
+      },
     ],
   },
-])
+]);
