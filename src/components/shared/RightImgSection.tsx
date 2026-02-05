769e60f44647d93080d86d0942d9044df23dd59c
AndresISouto <AndresISouto@users.noreply.github.com>
2026-02-05 10:09:54 +0100

    right image section

---
+++ src/components/shared/RightImgSection.tsx
@@ -0,0 +1,20 @@
+interface Props {
+  img: string;
+  title: string;
+  body: string;
+}
+
+export const RightImgSection = ({ img, title, body }: Props) => {
+  return (
+    <>
+      <section className="grid grid-cols-3  p-8 m-8  rounded-xl shadow-xl relative ">
+        <div className="absolute bg-blur opacity-10 inset-0 bg-cover bg-center"></div>
+        <article className="col-span-2 m-4 p-4 font-custom flex items-center flex-col justify-center ">
+          <h3 className="text-5xl p-4">{title}</h3>
+          <p className="leading-relaxed">{body}</p>
+        </article>
+        <img src={img} alt="esto es indescriptible..." className="col-span-1" />
+      </section>
+    </>
+  );
+};
