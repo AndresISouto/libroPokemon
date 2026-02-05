864226fa396a7610816815e360de4dc87f0cca9d
AndresISouto <AndresISouto@users.noreply.github.com>
2026-02-05 10:09:13 +0100

    glosarypage

---
+++ src/pages/GlosaryPage.tsx
@@ -0,0 +1,14 @@
+import { Footer } from "@/components/shared/Footer";
+import { LeftImgSection } from "@/components/shared/LeftImgSection";
+import { RightImgSection } from "@/components/shared/RightImgSection";
+
+export const GlosaryPage = () => {
+  return (
+    <section>
+      <LeftImgSection img="/mateo-bohemio.jpeg"></LeftImgSection>
+      <RightImgSection img="/mateo-bohemio.jpeg"></RightImgSection>
+      <LeftImgSection img="/mateo-bohemio.jpeg"></LeftImgSection>
+      <Footer></Footer>
+    </section>
+  );
+};
