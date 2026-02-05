df3f911b989ef108be521008078dec1ec46c2d5e
AndresISouto <AndresISouto@users.noreply.github.com>
2026-02-05 13:04:49 +0100

    size constrints

--- src/components/shared/LeftImgSection.tsx
+++ src/components/shared/LeftImgSection.tsx
@@ -7,9 +7,13 @@ interface Props {
 export const LeftImgSection = ({ img, title, body }: Props) => {
   return (
     <>
-      <section className="grid grid-cols-3  p-8 m-8  rounded-xl shadow-xl relative ">
+      <section className="grid grid-cols-3  p-8 m-8  rounded-3xl shadow-xl relative ">
         <div className="absolute bg-blur opacity-10 inset-0 bg-cover bg-center"></div>
-        <img src={img} alt="esto es indescriptible..." className="col-span-1" />
+        <img
+          src={img}
+          alt="esto es indescriptible..."
+          className="col-span-1 max-h-150 my-auto"
+        />
         <article className="col-span-2 m-4 p-4 font-custom flex items-center flex-col justify-center ">
           <h3 className="text-5xl p-4">{title}</h3>
           <p className="leading-relaxed">{body}</p>
