cc3b0f05fccb4c922a5d54ed0d9ea2b2babad96d
AndresISouto <AndresISouto@users.noreply.github.com>
2026-02-05 10:08:41 +0100

    redid nav links

---
+++ src/consts/links.ts
@@ -0,0 +1,55 @@
+export const navLinks = [
+  {
+    id: 1,
+    title: "Inicio",
+    href: "/",
+  },
+  {
+    id: 2,
+    title: "Pokemons",
+    href: "/pokemons",
+  },
+  {
+    id: 3,
+    title: "Personajes",
+    href: "/personajes",
+  },
+  {
+    id: 4,
+    title: "Mapa",
+    href: "/mapas",
+  },
+  {
+    id: 5,
+    title: "Lore",
+    href: "/lore",
+  },
+  {
+    id: 6,
+    title: "Libros",
+    href: "/libros",
+  },
+];
+
+export const glosaryLinks = [
+  {
+    id: 1,
+    title: "Pokemons",
+    href: "/glosario/pokemons",
+  },
+  {
+    id: 1,
+    title: "Personajes",
+    href: "/glosario/personajes",
+  },
+  {
+    id: 1,
+    title: "Mapas",
+    href: "/glosario/mapas",
+  },
+  {
+    id: 1,
+    title: "Lore",
+    href: "/glosario/lore",
+  },
+];
