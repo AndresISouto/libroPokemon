725d58915dbdf195387d66460abdca158524ed39
AndresISouto <AndresISouto@users.noreply.github.com>
2026-02-05 10:09:49 +0100

    navbar

--- src/components/shared/NavBar.tsx
+++ src/components/shared/NavBar.tsx
@@ -1,23 +1,41 @@
-import { NavLink } from 'react-router'
-import { navLinks } from '../../consts/links'
+import { NavLink } from "react-router";
+import { navLinks } from "../../consts/links";
 
 export const NavBar = () => {
   return (
-    <nav className='h-96 bg-amber-200'>
-      <h1>LOGO</h1>
-      <div>
-        {navLinks.map(link => (
-          <NavLink
-            key={link.id}
-            to={link.href}
-            className={({ isActive }) => (
-              isActive ? "text-cyan-600 underline" :
-                "text-gray-700 px-3 py-2 rounded-md text-sm font-medium" +
-                "hover:underline hover:text-cyan-500 transition-all duration-300"
-            )}
-          ></NavLink>
-        ))}
+    <nav className="h-26 bg-nav w-screen">
+      <div className="flex items-center justify-between">
+        <div className="flex items-center">
+          <img src="/logo.png" alt="logo" className="h-24 w-24 relative " />
+          <img
+            src="/title.png"
+            alt="Mi gran aventura pokemon Tirulina"
+            className="h-20"
+          />
+        </div>
+        <div className="mx-20">
+          {navLinks.map((link) =>
+            link.title !== "Glosario" ? (
+              <NavLink
+                key={link.id}
+                to={link.href}
+                className={({ isActive }) =>
+                  isActive
+                    ? "text-cyan-600 underline"
+                    : "text-white px-3 py-2 rounded-md text-sm font-medium" +
+                      " hover:text-cyan-500 transition-all duration-300"
+                }
+              >
+                {link.title}
+              </NavLink>
+            ) : (
+              <button className="text-white px-3 py-2 rounded-md text-sm font-medium hover:text-cyan-500 transition-all duration-300">
+                Glosario
+              </button>
+            ),
+          )}
+        </div>
       </div>
     </nav>
-  )
-}
+  );
+};
