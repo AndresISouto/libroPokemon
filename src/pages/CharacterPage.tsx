81eab2b2a5ad02e8eed32538a0f6e8477876d346
AndresISouto <AndresISouto@users.noreply.github.com>
2026-02-10 09:00:22 +0100

    add page sections

---
+++ src/pages/CharacterPage.tsx
@@ -0,0 +1,24 @@
+import { LeftImgSection } from "@/components/shared/LeftImgSection";
+import { RightImgSection } from "@/components/shared/RightImgSection";
+
+export const CharacterPage = () => {
+  return (
+    <>
+      <LeftImgSection
+        img="/transparent/Freya.png"
+        title=""
+        body=""
+      ></LeftImgSection>
+      <RightImgSection
+        img="/transparent/Nate2.png"
+        title=""
+        body=""
+      ></RightImgSection>
+      <LeftImgSection
+        img="/transparent/Rue.png"
+        title=""
+        body=""
+      ></LeftImgSection>
+    </>
+  );
+};
